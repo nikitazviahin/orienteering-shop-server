08aae32647f7df3c146c162fcdaf360c820e7ec8
mykyta <mykytazviahin@yahoo.com>
2021-11-16 15:03:33 +0200

    feat:database setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+require('dotenv').config()
 import express from "express";
+import db from "./db";
 
+const port = process.env.PORT || 5000;
 const app = express();
-const port = 5000;
+
+console.log(process.env.PORT)
+
+const start = async () => {
+  try {
+    await db.authenticate();
+    await db.sync();
+    app.listen(port, () => console.log(`Running on port ${port}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
 
 app.get("/", (_, res) => {
   res.status(200).send();
 });
-
-app.listen(port, () => console.log(`Running on port ${port}`));
